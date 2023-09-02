3d91933991cda9d5f555078acf3d840caaa114ab
Alanisgas <gaston.alanis@gmail.com>
2023-09-02 19:31:23 -0300

    Navbar
    Navbar hamburguesa

--- src/scripts/components/Navbar.js
+++ src/scripts/components/Navbar.js
@@ -1,4 +1,21 @@
 export const Navbar = ()=> {
-    let navbar = `<div class = "navbar"> ESTE ES EL NAVBAR</div>`
-    return navbar
+    return(`<header class="header">
+    <nav class="nav">
+      <a href="#" class="logo nav-link">WikiPotter</a>
+      <button class="nav-toggle" aria-label="Abrir menú">
+        <i class="fas fa-bars"></i>
+      </button>
+      <ul class="nav-menu">
+        <li class="nav-menu-item">
+          <a href="#" class="nav-menu-link nav-link">Personajes</a>
+        </li>
+        <li class="nav-menu-item">
+          <a href="#" class="nav-menu-link nav-link">Favoritos</a>
+        </li>
+        <li class="nav-menu-item">
+          <a href="#" class="nav-menu-link nav-link">Historial</a>
+        </li>
+      </ul>
+    </nav>
+  </header>`)
 }
--- src/scripts/containers/indexContainer.js
+++ src/scripts/containers/indexContainer.js
@@ -1,19 +1,21 @@
-import { ButtonCustom } from "../components/ButtonCustom.js";
 import { Navbar } from "../components/Navbar.js";
 import { Card } from "../components/Card.js";
-
 import { hpCharacters } from "./jsonHP.js";
+import { EfectoNavbar } from "../effects/EfectoNavbar.js";
 
 export const IndexRender = () => {
-    let root = document.getElementById("root");
-    root.innerHTML += ButtonCustom("Este es el boton");
-    root.innerHTML += Navbar()
-    root.innerHTML += Card("Harry Potter", "https://ik.imagekit.io/hpapi/harry.jpg","texto",2 );
-
-    hpCharacters.forEach(personaje => {
-        if(personaje.image!= ""){
-            root.innerHTML += Card(personaje.name,personaje.image,personaje.house,personaje.id);
-        }
-    });
-}
-
+  let root = document.getElementById("root");
+  root.innerHTML += Navbar();
+  EfectoNavbar();
+  let rootCartas = document.getElementById("contenedor-cartas");
+  hpCharacters.forEach((personaje) => {
+    if (personaje.image != "") {
+      rootCartas.innerHTML += Card(
+        personaje.name,
+        personaje.image,
+        personaje.house,
+        personaje.id
+      );
+    }
+  });
+};
