4a278ce731e56c4c7f33d4d0555c61466ef6980d
RodrigoLago <rilago@protonmail.com>
2023-09-04 00:00:57 -0300

    Actualizo el formato de las cartas

--- src/scripts/components/Card.js
+++ src/scripts/components/Card.js
@@ -1,13 +1,10 @@
 export const Card = (nombre,imagen,escuela,id) =>{
-    return `<div class="carta">
-                <div class="imagen-carta">
-                    <img src="${imagen}" class="avatar"/>
-                 </div>
-                <div class="nombre-carta">
-                    <h2>${nombre}</h2>
+    return `<article class="card">
+                <img class="card-img" src="${imagen}" />
+                <div class="overlay">
+                    <div class = "div-nombre">
+                        <h1>${nombre}</h1>
+                    </div>
                 </div>
-                <div class="escuela-carta">
-                    <h3>${escuela}</h3>
-                </div>
-            </div>`
+            </article>`
 }
