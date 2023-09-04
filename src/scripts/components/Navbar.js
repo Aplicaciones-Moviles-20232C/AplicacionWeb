fedbe9298459ce2168e256fae41baf3544c62892
Alanisgas <gaston.alanis@gmail.com>
2023-09-04 03:30:08 -0300

    imagen casas
    Agregue el logo de las casas de Hogwarts y metí los h2

--- src/scripts/components/Navbar.js
+++ src/scripts/components/Navbar.js
@@ -1,22 +1,31 @@
 export const Navbar = ()=> {
     return(`<header class="header">
     <nav class="nav">
-      <a href="#" class="logo nav-link">WikiPotter</a>
+    <img src="/src/img/casas.png" alt="hogwarts" class="img-casas">
+    <a href="#" class="logo nav-link" >WikiPotter</a>
       <button class="nav-toggle" aria-label="Abrir menú">
         <i class="fas fa-bars"></i>
       </button>
       <ul class="nav-menu">
         <li class="nav-menu-item">
+        <h2>
           <a href="#" class="nav-menu-link nav-link">Personajes</a>
-        </li>
+       </h2> 
+          </li>
         <li class="nav-menu-item">
+        <h2>
           <a href="#" class="nav-menu-link nav-link">Favoritos</a>
+          </h2>
         </li>
         <li class="nav-menu-item">
+        <h2>
           <a href="#" class="nav-menu-link nav-link">Historial</a>
+          </h2>
         </li>
         <li class="nav-menu-item">
+        <h2>
           <a href="#" class="nav-menu-link nav-link">Mas informacion</a>
+          </h2>
         </li>
       </ul>
     </nav>
