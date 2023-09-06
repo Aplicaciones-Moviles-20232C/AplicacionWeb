2d1f7a144d7ac5fc683e78211fa1e60c8dd51bfd
RodrigoLago <rilago@protonmail.com>
2023-09-06 18:40:15 -0300

    Elimino el boton de ejemplo

--- src/scripts/containers/indexContainer.js
+++ src/scripts/containers/indexContainer.js
@@ -1,4 +1,3 @@
-import { ButtonCustom } from "../components/ButtonCustom.js";
 import { Navbar } from "../components/Navbar.js";
 import { Card } from "../components/Card.js";
 
@@ -7,7 +6,6 @@ import { GetCharacters, GetCharacterById } from "../services/apiCall.js";
 
 export const IndexRender = () => {
     let root = document.getElementById("root");
-    root.innerHTML += ButtonCustom("Este es el boton");
     root.innerHTML += Navbar()
     let rootCartas = document.getElementById("contenedor-cartas")
     hpCharacters.forEach(personaje => {
@@ -15,6 +13,5 @@ export const IndexRender = () => {
             rootCartas.innerHTML += Card(personaje.name,personaje.image,personaje.house,personaje.id);
         }
     });
-
 }
 
