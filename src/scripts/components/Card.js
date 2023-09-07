9af97ada9a8326425db48d002c7232ceb7952f75
RodrigoLago <rilago@protonmail.com>
2023-09-06 21:20:17 -0300

    Arreglo cartas-navbar
    las cartas ya no pisan el navbar y corrijo el responsive del navbar.

--- src/scripts/components/Card.js
+++ src/scripts/components/Card.js
@@ -1,6 +1,8 @@
 export const Card = (nombre,imagen,escuela,id) =>{
     return `<article class="card">
-                <img class="card-img" src="${imagen}" />
+                <div class="div-img">
+                    <img class="card-img" src="${imagen}" />
+                </div>
                 <div class="overlay">
                     <div class = "div-nombre">
                         <h1>${nombre}</h1>
