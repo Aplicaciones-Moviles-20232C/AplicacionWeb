9e1e7f9f9642045ea5007bc82f12bc2568736cb4
Alanisgas <gaston.alanis@gmail.com>
2023-09-11 23:15:49 -0300

    Detail
    Trae la información, y se renderiza tiene los botones favorito y compartir

--- src/scripts/components/Detail.js
+++ src/scripts/components/Detail.js
@@ -1,16 +1,27 @@
 export const Detail = (nombre,imagen,escuela,id,especie, color) =>{
-    return `<article class="detail">
+    return `<article class="contenedor-detail">
+            <div class="detail ${color}">
                 <img class="detail-img" src="${imagen}" />
-                <div class="overlay">
-                    <div class = "div-nombre">
-                        <h1>${nombre}</h1>
+                <div class="overlay ">
+                    <div class="info ${color}">
+                        <div class = "div-nombre">
+                            <h1>Nombre: ${nombre}</h1>
+                        </div>
+                        <div class = "div-escuela ">
+                            <h1>Casa:${escuela}</h1
+                        </div>
+                        <div class = "div-especie">
+                            <h1>Especie:${especie}</h1>
+                          
                     </div>
-                    <div class = "div-escuela ${color}">
-                        <h1>${escuela}</h1
-                    </div>
-                    <div class = "div-especie">
-                    <h1>${especie}</h1
-                    </div>
+
                 </div>
+                </div>
+                <img src="/src/img/83976.png" alt="favoritos" class="img-favoritos">
+                <img src="/src/img/compartir.png" alt="compartir" class="img-compartir">
+                </div>
+                
+                
+                
             </article>`
 }
