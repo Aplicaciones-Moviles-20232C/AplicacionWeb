2580e389ac51fc7d254c838d12f7e805f98eae57
RodrigoLago <rilago@protonmail.com>
2023-09-17 19:47:16 -0300

    Renderización de favoritos en html

--- src/scripts/containers/favoritosContainer.js
+++ src/scripts/containers/favoritosContainer.js
@@ -1,7 +1,16 @@
 import { Navbar } from "../components/Navbar.js"
 import { EfectoNavbar } from "../effects/EfectoNavbar.js";
+import { Card } from "../components/Card.js";
 export function FavoritosRender(){
     $("#root").html(Navbar(false))
     EfectoNavbar()
+    RenderizarFavoritos()
+}
 
+
+function RenderizarFavoritos() {
+    let personajes = JSON.parse(localStorage.getItem("Favoritos"))
+    personajes.forEach(personaje => {
+        $("#contenedor-cartas").append(Card(personaje.name,personaje.image,personaje.house,personaje.id))
+    });
 }
