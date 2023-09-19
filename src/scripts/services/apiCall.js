3c26c1334185c1d669ee3bf4273bf6d09bedbc28
RodrigoLago <rilago@protonmail.com>
2023-09-19 02:17:25 -0300

    Elimino codigo comentado

--- src/scripts/services/apiCall.js
+++ src/scripts/services/apiCall.js
@@ -30,16 +30,6 @@ export const GetCharacterById = (id, callback) =>{
 }
 
 export const FilterCharacter = (searchTerm, callback) =>{
-  /*$.get( `${baseUrl}`, function( data ) {
-    //Si el termino que recibo esta vacio, entonces le retorno todos los personajes. Sino hago el filtrado
-    searchTerm=="" ? localStorage.setItem("personajes", JSON.stringify(data)) : localStorage.setItem("personajes", buscarPersonajesSimilares(searchTerm, data));
-    callback()
-  }).fail(function (jqXHR, textStatus, errorThrown) {
-    // En caso de error, retorno la lista almacenada localmente
-    searchTerm=="" ? localStorage.setItem("personajes", JSON.stringify(hpCharacters)) : localStorage.setItem("personajes", buscarPersonajesSimilares(searchTerm, hpCharacters)); 
-    callback()
-  });*/
-  console.log("hola")
   let data = JSON.parse(localStorage.getItem("personajes"))
   searchTerm == "" ? data = data : data = buscarPersonajesSimilares(searchTerm, data);
   localStorage.setItem("busqueda", JSON.stringify(data))
