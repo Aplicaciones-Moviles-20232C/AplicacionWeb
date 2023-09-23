b02f79c7e9a2224193f1d3a80e9844f0613019a9
RodrigoLago <rilago@protonmail.com>
2023-09-23 18:20:35 -0300

    Footer al pie de cada pagina

--- src/scripts/components/Footer.js
+++ src/scripts/components/Footer.js
@@ -1,7 +1,7 @@
 export const FooterContent = () =>{
     return `
     <a href="../../../src/views/index.html">
-        <h3>&copy; Copyright 2023 WikiPotter</h3>
+        <h3>&copy; Copyright 2023 WikiPotter || Alanis Gastón y Lago Rodrigo</h3>
         </a>
     `
 }
